8cc6b7656281a1d6a5cf170184d8ac9bee69cac0
pctjose <pctjose@gmail.com>
2014-08-26 15:45:28 +0200

    Resolver conflito com reset

--- carteiraprofissional/src/main/java/org/apm/carteiraprofissional/view/requisicao/CompletarRequisiaoVM.java
+++ carteiraprofissional/src/main/java/org/apm/carteiraprofissional/view/requisicao/CompletarRequisiaoVM.java
@@ -91,8 +91,6 @@ public class CompletarRequisiaoVM extends SelectorComposer<Component> {
 
 		requisicao = (Requisicao) Sessions.getCurrent().getAttribute(
 				"requisicao");
-		
-		//logedInUser= (Utilizador)Sessions.getCurrent().getAttribute("logedIn");
 		logedInUser= (Utilizador)Sessions.getCurrent().getAttribute("utilizador");
 
 		if (requisicao != null) {
